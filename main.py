b3677781451fab2d4edb833a5242203fbafaa43d
HardRope <61661973+HardRope@users.noreply.github.com>
2022-06-29 05:47:31 +0300

    del unuseful func

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ from dotenv import load_dotenv
 from vk_image_loading import get_upload_link, upload_image, save_uploaded_photo, publicate_comics
 from xkcd_comics_loading import get_comic_info, download_comic, get_comics_count, choose_random_comics
 
-def create_directory(main_dir ='image'):
-    file_path = Path.cwd() / main_dir
-    Path.mkdir(file_path, parents=True, exist_ok=True)
-    return file_path
-
 
 if __name__ == '__main__':
     load_dotenv()
@@ -25,7 +20,7 @@ if __name__ == '__main__':
     author_comment = comic['alt']
     image_url = comic['img']
     image_filename = str(comic_num) + '.png'
-    image_path = create_directory() / image_filename
+    image_path = Path.cwd() / image_filename
 
     download_comic(image_url, image_path)
 
