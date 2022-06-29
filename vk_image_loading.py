40b348295ee57fda40d0c4a97434212911dba7f9
HardRope <61661973+HardRope@users.noreply.github.com>
2022-06-29 05:42:28 +0300

    replace funtions for upload and publish image to VK

---
+++ vk_image_loading.py
@@ -0,0 +1,51 @@
+import requests
+
+def get_upload_link(token, group_id, api_version):
+    url = f'https://api.vk.com/method/photos.getWallUploadServer'
+    params = {
+        'group_id': group_id,
+        'access_token': token,
+        'v': api_version,
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()['response']['upload_url']
+
+
+def upload_image(url, path):
+    with open(path, 'rb') as file:
+        files = {
+            'photo': file
+        }
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+    return response.json()
+
+
+def save_uploaded_photo(group_id, token, api_version, saved_data):
+    url = f'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {
+        'group_id': group_id,
+        'access_token': token,
+        'v': api_version,
+        'server': saved_data['server'],
+        'photo': saved_data['photo'],
+        'hash': saved_data['hash'],
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()['response'][0]
+
+
+def publicate_comics(group_id, token, api_version, message, media_id, owner_id):
+    url = f'https://api.vk.com/method/wall.post'
+    params = {
+        'owner_id': f'-{group_id}',
+        'access_token': token,
+        'v': api_version,
+        'message': message,
+        'attachments': f'photo{owner_id}_{media_id}'
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
