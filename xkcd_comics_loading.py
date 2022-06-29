753392885bc39e55ce7c7020d88b47c33c0d339c
HardRope <61661973+HardRope@users.noreply.github.com>
2022-06-29 05:42:50 +0300

    replace functions to choose and download comic

---
+++ xkcd_comics_loading.py
@@ -0,0 +1,25 @@
+from random import randint
+
+import requests
+
+def get_comics_count():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()['num']
+
+def choose_random_comics(count):
+    return randint(1, count)
+
+def get_comic_info(number):
+    url = f'https://xkcd.com/{number}/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def download_comic(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
