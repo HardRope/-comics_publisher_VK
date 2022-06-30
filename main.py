1bb4c58d6aae068b5e7507484896ad9cbef982bb
HardRope <61661973+HardRope@users.noreply.github.com>
2022-06-30 18:44:06 +0300

    add try-finally block to delete image

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from dotenv import load_dotenv
 from vk_image_publishing import get_upload_link, upload_image, save_uploaded_photo, publish_comics
 from xkcd_comics_loading import get_comic_info, download_comic, get_comics_count, choose_random_comic
 
-
 if __name__ == '__main__':
     load_dotenv()
 
     vk_access_token = os.getenv('VK-ACCESS-TOKEN')
     vk_group_id = os.getenv('VK-GROUP-ID')
     vk_api_version = os.getenv('VK-API-V')
+
+    upload_link = get_upload_link(
+        vk_access_token,
+        vk_group_id,
+        vk_api_version
+    )
 
     comics_count = get_comics_count()
     comic_num = choose_random_comic(comics_count)
@@ -24,15 +29,12 @@ if __name__ == '__main__':
     image_filename = str(comic_num) + '.png'
     image_path = Path.cwd() / image_filename
 
-    download_comic(image_url, image_path)
+    try:
+        download_comic(image_url, image_path)
+        upload_image_result = upload_image(upload_link, image_path)
+    finally:
+        os.remove(image_path)
 
-    upload_link = get_upload_link(
-        vk_access_token,
-        vk_group_id,
-        vk_api_version
-    )
-
-    upload_image_result = upload_image(upload_link, image_path)
     image_server, image_photo, image_hash = upload_image_result.values()
 
     loaded_photo = save_uploaded_photo(
@@ -52,4 +54,3 @@ if __name__ == '__main__':
         loaded_photo['owner_id'],
         vk_api_version,
     )
-    os.remove(image_path)
